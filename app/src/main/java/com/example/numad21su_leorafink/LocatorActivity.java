c54eedaf02de323c85c70a4a275d6b1fcf09b483
leorafink1 <fink.le@husky.neu.edu>
2021-06-11 16:36:44 -0400

    locator front end

--- app/src/main/java/com/example/numad21su_leorafink/LocatorActivity.java
+++ app/src/main/java/com/example/numad21su_leorafink/LocatorActivity.java
@@ -1,14 +1,29 @@
 package com.example.numad21su_leorafink;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LocatorActivity extends AppCompatActivity {
-  //  @Override
-  //  protected void onCreate(Bundle savedInstanceState) {
- //       super.onCreate(savedInstanceState);
-//        setContentView(R.layout.contentbuttons);
+    Button locatorButton;
+    TextView longitude;
+    TextView latitude;
 
-  //  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.locatorpage);
+
+        locatorButton = findViewById(R.id.button5);
+        latitude = findViewById(R.id.latitude);
+        longitude = findViewById(R.id.longitude);
+
+
+
+
+    }
+
+
 }
